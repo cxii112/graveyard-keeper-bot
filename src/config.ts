6c4b7390ed3314bb1082d85bd33f27759114980d
cxii112 <lyakhovoy.d@ya.ru>
2022-09-04 19:39:00 +0500

    delete unused params

--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,9 @@ import dotenv from "dotenv";
 export function load() {
   dotenv.config();
   return {
-    PORT: process.env.PORT || "3000",
     DISCORD_TOKEN: process.env.DISCORD_TOKEN,
     GUILD_ID: process.env.GUILD_ID,
     CHANNEL_ID: process.env.CHANNEL_ID,
     ROLE_ID: process.env.ROLE_ID,
-    TIME_INTERVAL: process.env.TIME_INTERVAL
   };
 }
